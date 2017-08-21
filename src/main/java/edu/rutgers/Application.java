4591c80aba9d105b529e40efbf88a3fbcef46d92
sumasrijakommuri <sumasrija@gmail.com>
2017-08-21 18:48:31 -0400

    feat:tetsing merge

--- src/main/java/edu/rutgers/Application.java
+++ src/main/java/edu/rutgers/Application.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.*;
 // This is our new project
 
+//This is to test Merge
 @EnableWebMvc
 @ComponentScan
 public class Application {
