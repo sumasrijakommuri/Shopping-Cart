3a753bcb6ac2d802a55bc7dbef972ca42773f23f
kiranjatty37 <kj272@scarletmail.rutgers.edu>
2017-08-21 15:39:36 -0700

    feat:firstpushtest

--- src/main/java/edu/rutgers/Application.java
+++ src/main/java/edu/rutgers/Application.java
@@ -2,6 +2,7 @@ package edu.rutgers;
 
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.*;
+// This is our new project
 
 @EnableWebMvc
 @ComponentScan
